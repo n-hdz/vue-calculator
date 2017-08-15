b4e8968fa7a002c51c3d068a772c9d4fe7bf5028
n-hdz <inhc.dev@gmail.com>
2017-08-15 18:34:16 -0500

    script re-write Updated => Calc now auto evaluates, auto flushes and stacks operations TBD => auto-evaluate when pushing results

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
+//Re-work
 var app = new Vue({
   el: "#calc",
   delimiters: ["${", "}"],
   data: {
-    dec: 0,
-    float: false,
-    memory: 0,
-    type: "",
+    memory: [],
     operands: [],
+    operand: "",
+    operator: "",
     print_out: "",
-    input: "",
-    operated: false,
+    evaluated: false,
     result: 0
   },
 
@@ -20,146 +19,89 @@ var app = new Vue({
       this.print_out = "THANK YOU! BE SURE TO STAR ON GITHUB!";
     },
 
-    clearNum: function() {
-      this.dec = 0;
-      this.float = false;
-      this.memory = 0;
-      this.type = "";
-      this.operands = [];
-      this.print_out = "";
-      this.input = "";
-      this.operated = false;
-      this.result = 0;
+    clearNum: function() {;
     },
 
     erraseNum: function() {
-      this.clearNum();
     },
 
     getNum: function(n) {
-      //if integers
-      if (!this.float) {
-        //commit integer to memory
-        this.memory = (this.memory * 10) + n;
-        this.input = this.memory.toString();
-        this.printOut(this.input);
-        //if floats above .000
-      } else if (this.float) {
-        this.memory = ((this.memory * this.dec) + n) / this.dec;
-        this.input = this.memory.toString();
-        this.printOut(this.input);
-        this.dec *= 10;
-        if (this.dec >= 100000) {
-        this.input = "MAX. DECIMAL ERROR";
-        this.printOut(this.input);
+      this.memory.push(n);
+      this.print_out += n.toString();
+    },
+    
+    pushOperand: function(memory, operator){
+      this.operand = this.memory.join('');
+      if(this.operator !== ""){
+        this.operands.push(this.operand.toString(), this.operator);
+        if(this.operands.length >= 3) {
+          this.operands.pop();
+          this.evalOperands();
         }
+      } else { 
+        this.operands.push(this.operand.toString());
       }
-      
-    },
-    
-    makeFloat: function() {
-      this.float = true;
-      this.dec += 10;
-    },
-    
-    printOut: function(input){
-      this.print_out = input;
-    },
-    
-    pushOperand: function(){
-      if(!this.operated){
-        //set second operator from memory
-        this.operands.push(this.memory);
-        console.log("Push level: " + this.operands + " Type: " + this.type);
-      } else {
-        //on flushed memory, set result as first operand
-        this.operands[0] = Number(this.result);
-        console.log("Push level: " + this.operands + " Type: " + this.type);
-        this.operated = false;
-      }
-    },
-    
-    sumOp: function() {
-        //Set operator type, reflect on display
-        this.type = "sum";
-        this.print_out += " + ";
-        //push seconf operand argument and flush memory
-        this.pushOperand();
-        this.flushMem();
-    },
-
-    subOp: function() {
-      //Set operator type, reflect on display
-      this.type = "sub";
-      this.print_out += " - ";
-      //push seconf operand argument and flush memory
-      this.pushOperand();
+      console.log("In operands at call: " + this.operands + " Length: " + this.operands.length);
+      if(this.evaluated){this.operands.push(this.result.toString(), this.operator)}
+      console.log("In operands after call: " + this.operands + " Length: " + this.operands.length);
       this.flushMem();
     },
-
-    timesOp: function() {
-      //Set operator type, reflect on display
-      this.type = "mult";
-      this.print_out += " * ";
-      //push seconf operand argument and flush memory
-      this.pushOperand();
-      this.flushMem();
-    },
-
-    divideOp: function() {
-      //Set operator type, reflect on display
-      this.type = "div";
-      this.print_out += " / ";
-      //push seconf operand argument and flush memory
-      this.pushOperand();
-      this.flushMem();
+    
+    switchOperator: function(op){
+      switch(op){
+          case('add'):
+          this.operator = " + ";
+          this.pushOperand(this.operator);
+          this.print_out += this.operator;
+          break;
+          
+          case('sub'):
+          this.operator = " - ";
+          this.pushOperand(this.operator);
+          this.print_out += this.operator;
+          break;
+          
+          case('mult'):
+          this.operator = " * ";
+          this.pushOperand(this.operator);
+          this.print_out += this.operator;
+          break;
+          
+          case('div'):
+          this.operator = " / ";
+          this.pushOperand(this.operator);
+          this.print_out += this.operator;
+          break;
+          
+        default:
+          break;
+               }
     },
 
     getResult: function() {
-      this.pushOperand();
-      //operate
-      switch (this.type) {
-        case "sum":
-          //Add
-          this.result = this.operands.reduce(function(sum, value){return sum + value});
-          this.flushOp();
-          break;
-        case "sub":
-          //Substract
-          this.result = this.operands.reduce(function(sum, value){return sum - value});
-          this.flushOp();
-          break;
-        case "mult":
-          //Multiply
-          this.result = this.operands.reduce(function(sum, value){return sum * value});
-          this.float ? this.result.toFixed(3) : this.result;
-          this.flushOp();
-          break;
-        case "div":
-          //Divide
-          this.result = this.operands.reduce(function(sum, value){return sum / value}).toFixed(3);
-          this.flushOp();
-          break;
-        default:
-          this.result = "NAN";
-          this.print_out = "OPERATION TYPE ERROR";
-          break;
-      }
+     //empty operator position
+      this.operator = "";
+      //push last operand in memory
+      this.pushOperand(this.memory);
+      this.evalOperands();
+    },
+    
+    evalOperands: function(){
+      console.log(this.operands);
+      this.result = eval(this.operands.join(''));
+      this.flushOp();
+      this.flushMem();
+      this.print_out = this.result.toString();
+      this.evaluated = true
     },
     
 //flushers
     flushMem: function (){
-      //flush memory
-      this.memory = 0;
-      this.dec = 0;
-      this.float = false;
+      this.memory = [];
     },
     
     flushOp: function(){
-      //flush operands memory
       this.operands = [];
-      this.operated = true;
     }
-    
   }
 });
